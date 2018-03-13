1a25bc1f6760e53837272a19986c9ab0be15f64f
lamchuong28 <lamchuong28@gmail.com>
2018-03-14 00:05:58 +0700

    commit changed

---
+++ app/src/main/java/lamchuong/com/baitapgit2/LoginActivity.java
@@ -0,0 +1,10 @@
+package lamchuong.com.baitapgit2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CHUONGLAM on 13/03/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/lamchuong/com/baitapgit2/Student.java
@@ -0,0 +1,8 @@
+package lamchuong.com.baitapgit2;
+
+/**
+ * Created by CHUONGLAM on 14/03/2018.
+ */
+
+public class Student {
+}
